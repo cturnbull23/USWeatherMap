dbed4fb0ecc69b03c76b35c738c8787a9f8e43e8
Christopher Turnbull <76788494+cturnbull23@users.noreply.github.com>
2025-05-04 19:06:36 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -73,7 +73,7 @@ function init() {
 
     // Storm Prediction Center Day 1 Categorical Outlook
     var spcCategorical = L.esri.featureLayer({
-        url: 'https://mapservices.weather.noaa.gov/vector/rest/services/outlooks/SPC_wx_outlks/FeatureServer/0',
+        url: 'https://mapservices.weather.noaa.gov/vector/rest/services/outlooks/SPC_wx_outlks/FeatureServer',
         style: function(feature) {
             var outlookColor = 'gray'; // if unknown
             switch (feature.properties.label) {
